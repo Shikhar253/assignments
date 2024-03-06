80e06550b315b138e93ab64e21ec85da441f8d5e
Shikhar Saxena <saxenashikhar01@gmail.com>
2024-03-05 20:13:01 -0500

    anagrams.js done

--- 01-js/easy/anagram.js
+++ 01-js/easy/anagram.js
@@ -5,7 +5,43 @@
 */
 
 function isAnagram(str1, str2) {
+  str1 = str1.toLowerCase().split("").sort().join("");
+  str2 = str2.toLowerCase().split("").sort().join("");
 
+  // OR
+  // str1 = JSON.stringify(str1.toLowerCase().split("").sort());
+  // str2 = JSON.stringify(str2.toLowerCase().split("").sort());
+
+  // JSON.stringify array of strings kar dega input ko whereas w/o uske array of chars rahega wo
+  // like this:
+  // ["e","h","h","u"]
+  // [ 'e', 'h', 'h', 'u' ]
+
+  return str1 == str2 ? true : false;
 }
 
 module.exports = isAnagram;
+
+// when it comes to objects and arrays, === behaves differently. It checks whether the two operands reference the same object or array in memory, not just whether they have the same properties or elements. This is known as reference equality.
+
+// For example:
+
+// javascript
+// Copy code
+// let obj1 = { key: 'value' };
+// let obj2 = { key: 'value' };
+
+// console.log(obj1 === obj2); // Output: false
+// Even though obj1 and obj2 have the same properties and values, === returns false because they reference different objects in memory.
+
+// Similarly, for arrays:
+
+// javascript
+// Copy code
+// let arr1 = [1, 2, 3];
+// let arr2 = [1, 2, 3];
+
+// console.log(arr1 === arr2); // Output: false
+// Here, arr1 and arr2 contain the same elements, but they are different arrays in memory, so === returns false.
+
+// To compare the contents of two objects or arrays in JavaScript, you need to manually compare their properties or elements, which usually involves iterating over them and performing comparisons on each property or element. Alternatively, you can convert them to strings using JSON.stringify() as mentioned earlier.
--- week-1/offline-class-1/level-2/01-Class.js
+++ week-1/offline-class-1/level-2/01-Class.js
@@ -1,12 +1,29 @@
-
 class Animal {
   constructor(name, legCount) {
-    this.name = name
-    this.legCount = legCount
+    this.name = name;
+    this.legCount = legCount;
   }
   describe() {
-    return `${this.name} has ${this.legCount} legs`
+    return `${this.name} has ${this.legCount} legs`;
   }
 }
 
+class Vehicle {
+  constructor(numOfWheels, weight, type) {
+    this.numOfWheels = numOfWheels;
+    this.weight = weight;
+    this.type = type;
+  }
+  findType() {
+    if (this.numOfWheels == 2) this.type = "bike";
+    return this.type;
+  }
 
+  static funcName() {
+    return "huehueh";
+  }
+}
+
+let bike = new Vehicle(2, 200, "hueh");
+console.log(bike.findType());
+console.log(Vehicle.funcName());
